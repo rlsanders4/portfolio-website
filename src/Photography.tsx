a573e0cc21efbb39f3e69cede6e9258987b33f33
Ralph Sanders <rlsanders4@gmail.com>
2024-04-04 19:26:04 -0400

    Added photography

--- src/Photography.tsx
+++ src/Photography.tsx
@@ -1,8 +1,93 @@
+import pic from './assets/cactus.jpg'
+import { useState } from 'react'; 
+
 function Photography() {
+
+  const [lightboxOpen, setLightboxOpen] = useState(false);
 
   return (
     <>
+      <div>
+        <div className="text-center m-auto py-16">
+          <h1 className="font-bold text-7xl">Photography</h1>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-8 px-16" onContextMenu={(e)=> e.preventDefault()}>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+          <img className="transition ease-in-out duration-100 border-2 hover:scale-105" src={pic} onClick={() => setLightboxOpen(!lightboxOpen)}/>
+        </div>
+        <div className="text-center pb-8">
+          <p>© 2024 Ralph Sanders. All rights reserved.</p>
+        </div>
+      </div>
+      <div onClick={() => setLightboxOpen(!lightboxOpen)} className={lightboxOpen ? "bg-black/90 fixed w-full h-full top-0 left-0 z-10 flex flex-col m-0 justify-center center items-center" : "hidden"}>
+        <div className="flex flex-col items-center justify-center">
+          <div onClick={(e) => e.stopPropagation()} className="grow flex items-end p-8 justify-center">
+            <img className="border-4 object-contain max-h-[70vh]" src={pic} onContextMenu={(e)=> e.preventDefault()}/>
+          </div>
+          <div className="flex-none w-14 h-14 flex items-start justify-center">
+          <button
+            onClick={() => setLightboxOpen(!lightboxOpen)}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg hover:bg-white/40 focus:outline-none focus:ring-2 focus:ring-gray-200">
+              <svg
+                  className="h-24 w-24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                <line x1="22" y1="22" x2="2" y2="12" />
+                <line x1="2" y1="12" x2="22" y2="2" />
 
+              </svg>
+            </button>
+            <button
+            onClick={() => setLightboxOpen(!lightboxOpen)}
+            className="inline-flex items-center p-2 mx-16 w-10 h-10 justify-center text-sm rounded-lg hover:bg-white/40 focus:outline-none focus:ring-2 focus:ring-gray-200">
+              <svg
+                  className="h-24 w-24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                <line x1="22" y1="2" x2="2" y2="22" />
+                <line x1="2" y1="2" x2="22" y2="22" />
+              </svg>
+            </button>
+            <button
+            onClick={() => setLightboxOpen(!lightboxOpen)}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg hover:bg-white/40 focus:outline-none focus:ring-2 focus:ring-gray-200">
+              <svg
+                  className="h-24 w-24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                <line x1="2" y1="22" x2="22" y2="12" />
+                <line x1="22" y1="12" x2="2" y2="2" />
+
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -5,27 +5,49 @@ function Projects() {
       <div className="text-center m-auto py-16">
         <h1 className="font-bold text-7xl">Projects</h1>
       </div>
-      <div className="w-3/4 m-auto p-4 grid grid-cols-1 gap-8">
-        <div className="border rounded p-4 bg-white/10">
-          <h3 className="font-bold text-3xl">Daras</h3>
+      <div className="w-3/4 m-auto px-4 pt-4 pb-16 grid grid-cols-1 gap-8">
+        <div className="border rounded p-4 bg-zinc-900">
+          <h3 className="font-bold text-4xl">Daras</h3>
           <p className="pb-2 italic">Kotlin</p>
-          <p className="pb-2">Daras is an app for leaning arabic.The website you are on now!The website you are on now!The website you are on now!The website you are on now!The website you are on now!</p>
-          <div>
-            <button className="pr-4">View Source</button>
-            <button>View in Play Store</button>
+          <p className="pb-2">Daras is an Android app to help you learn Arabic. 
+          This app serves as a quick reference guide for common grammer and vocabulary topics, such as the alphabet, numbers, pronouns, and verb conjugations. 
+          I was inspired to create this app while taking Arabic classes in college. 
+          It consolidates and conveniently displays key concepts for any Arabic language learner.
+          </p>
+          <div className="col-span-1 md:col-span-2">
+            <a href="https://github.com/rlsanders4/daras" target="_blank" rel="noopener noreferrer">
+              <button className="font-bold px-4 py-2 my-2 mr-4 border rounded hover:border-transparent hover:bg-white hover:text-zinc-900">View Source</button>
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.ralphsanders.daras&pcampaignid=web_share" target="_blank" rel="noopener noreferrer">
+              <button className="font-bold px-4 py-2 my-2 mr-4 border rounded hover:border-transparent hover:bg-white hover:text-zinc-900">View in Play Store</button>
+            </a>
           </div>
         </div>
-        <div className="border rounded p-4 bg-white/10">
-          <h3 className="font-bold text-3xl">Portfolio Website</h3>
-          <p className="pb-2 italic">React, JavaScript, HTML, Tailwind CSS</p>
-          <p className="pb-2">The website you are on now!The website you are on now!The website you are on now!The website you are on now!The website you are on now!</p>
-          <button>View Source</button>
+        <div className="border rounded p-4 bg-zinc-900">
+          <h3 className="font-bold text-4xl">Portfolio Website</h3>
+          <p className="pb-2 italic">React, TypeScript, HTML, Tailwind CSS</p>
+          <p className="pb-2">This is the website that you are on right now. 
+          It's built using multiple frontend technologies. 
+          React is used to build different components, and the React Router is used to switch between pages. 
+          Tailwind CSS is used to provide a utility-based approach to styling. 
+          The interactive elements on the site are designed with TypeScript.
+          </p>
+          <a href="https://github.com/rlsanders4/portfolio-website" target="_blank" rel="noopener noreferrer">
+              <button className="font-bold px-4 py-2 my-2 mr-4 border rounded hover:border-transparent hover:bg-white hover:text-zinc-900">View Source</button>
+          </a>
         </div>
-        <div className="border rounded p-4 bg-white/10">
-          <h3 className="font-bold text-3xl">Cron Jobs</h3>
-          <p className="pb-2 italic">Python</p>
-          <p className="pb-2">The website you are on now!The website you are on now!The website you are on now!The website you are on now!The website you are on now!</p>
-          <button>View Source</button>
+        <div className="border rounded p-4 bg-zinc-900">
+          <h3 className="font-bold text-4xl">Cron Jobs</h3>
+          <p className="pb-2 italic">Python, HTML, CSS</p>
+          <p>This project includes several scripts that I have configured as cron jobs on my home linux server.</p>
+          <ul className="pl-2 pb-2">
+            <li>- <code>newsletter.py</code>: Generate and email a configurable daily morning report containing the weather forecast, stock prices, and the top news articles.</li>
+            <li>- <code>email_wifi_report.py</code>: Generate and email a weekly Wi-Fi Speed Report with a daily breakdown of average download and upload speeds.</li>
+            <li>- <code>system_update.py</code>: Automatically keep your system up to date.</li>
+          </ul>
+          <a href="https://github.com/rlsanders4/cron-jobs" target="_blank" rel="noopener noreferrer">
+              <button className="font-bold px-4 py-2 my-2 mr-4 border rounded hover:border-transparent hover:bg-white hover:text-zinc-900">View Source</button>
+          </a>
         </div>
       </div>
     </>
